d57c04e5e7b09638490f0f60d17a8f9723b90d82
robertjosephderouin <robertjosephderouin@gmail.com>
2022-11-21 21:21:19 -0700

    added more css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import cardInfo from './Data';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -12,28 +13,27 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-      <h1>Robert DeRouin</h1>
-      <a href="https://www.linkedin.com/in/robert-derouin/">LinkedIn</a>
-      <h2>Software Engineer</h2>
-      <p>I am a software engineer looking to change my community for the better
-through superior technology. I am well versed in JavaScript, HTML, CSS,
-SASS, React & Cypress. For fun I am a world class miniature painter
-and I coach Brazilian Jiu-Jitsu!</p>
-      <h3>Techincal Expertise</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SASS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>GIT</li>
-          <li>GitHub</li>
-          <li>Cypress</li>
-          <li>Mocha/Chai</li>
-        </ul>
+      <main>
+        <h1>Robert DeRouin</h1>
+        <h2>Software Engineer</h2>
+        <a href="https://www.linkedin.com/in/robert-derouin/">LinkedIn</a>
+        <p>I am a software engineer looking to change my community for the better
+  through superior technology. I am well versed in JavaScript, HTML, CSS,
+  SASS, React & Cypress. I have a passion for front end development and learning new skills.</p>
+        <h3>Techincal Expertise</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>GIT</li>
+            <li>GitHub</li>
+            <li>Cypress</li>
+            <li>Mocha/Chai</li>
+          </ul>
         <Projects projects={this.state.projects}/>
-      </div>
+      </main>
     )
   }
 }
--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,13 @@ import './Card.css';
 
 const Card = ({ title, description, id, technologies, link }) => {
     return (
-      <div className='card'>
+      <div className='card css-box-shadow'>
         <h3>{title}</h3>
         <ul>
           {technologies.map(technology => <li key={technology}>{technology}</li>)}
         </ul>
         <p>{description}</p>
-        <a href={link}>Visit</a>
+        <a className='visit' href={link}>Visit</a>
       </div>
     )
   }
